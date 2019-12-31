cc2f7c1b91c69760be917e9cc5b2895ebdf0c479
Martin Yeung <myeung@finfabrik.com>
2019-12-31 21:19:38 +0800

    Server: make redis password configurable

--- src/server/queue.ts
+++ src/server/queue.ts
@@ -8,6 +8,7 @@ class Queue{
       redis: {
         port: process.env.REDIS_PORT || 6379,
         host: process.env.REDIS_HOST || 'localhost',
+        password: process.env.REDIS_PASSWORD || undefined,
       }
     });
   }
--- src/server/redis.ts
+++ src/server/redis.ts
@@ -3,6 +3,7 @@ const Redis = require('ioredis');
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+  password: process.env.REDIS_PASSWORD || null,
 });
 
 export default redis;
